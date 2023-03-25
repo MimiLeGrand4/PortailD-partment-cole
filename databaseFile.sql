a2670878176c5dcde3e5425b58e5e4c6f52325fb
Jérémy Cormier1 <jcormier1@dept-info.crosemont.quebec>
2023-03-25 13:01:15 -0400

    Base de donnee modifier et schema(dessin) de la base

--- databaseFile.sql
+++ databaseFile.sql
@@ -14,6 +14,7 @@ CREATE TABLE tbl_user (
     prenom varchar(35),
     accountType_id int,
     passwd varchar(99),
+    tuteur Boolean,
 
     PRIMARY KEY (user_id),
     FOREIGN KEY (accountType_id) REFERENCES tbl_accountType(accountType_id)
@@ -23,18 +24,56 @@ CREATE TABLE tbl_projet (
     projet_id int,
     nom varchar(50),
     user_id int,
-    postStatus_id int,
+    statut Boolean,
 
     PRIMARY KEY (projet_id),
     FOREIGN KEY (user_id) REFERENCES tbl_user(user_id)
+);
+
+CREATE TABLE tbl_evaluation(
+    user_id int,
+    projet_id int,
+    note int(3),
+    commentaire text
+
+
+    CONSTRAINT PK_tbl_evaluation PRIMARY KEY
+    (
+        user_id,
+        projet_id 
+    ),
+    FOREIGN KEY (projet_id) REFERENCES tbl_projet(projet_id)
+    FOREIGN KEY (user_id) REFERENCES tbl_user(user_id)
+);
+
+CREATE TABLE tbl_acces(
+    acces_id int,
+    nom varchar(30),
+
+    PRIMARY KEY (acces_id)
+);
+
+CREATE TABLE tbl_invite(
+    user_id int,
+    projet_id int,
+    acces_id int,
+
+    CONSTRAINT PK_tbl_invite PRIMARY KEY
+    (
+        user_id,
+        projet_id 
+    ),
+    FOREIGN KEY (projet_id) REFERENCES tbl_projet(projet_id)
+    FOREIGN KEY (user_id) REFERENCES tbl_user(user_id)
+    FOREIGN KEY (acces_id) REFERENCES tbl_acces(acces_id)
 );
 
 CREATE TABLE tbl_post (
     post_id int,
     projet_id int,
     caption varchar(50),
-    fichier varchar(100),
     user_id int,
+    moment datetime,
 
     CONSTRAINT PK_tbl_post PRIMARY KEY
     (
@@ -45,9 +84,30 @@ CREATE TABLE tbl_post (
     FOREIGN KEY (user_id) REFERENCES tbl_user(user_id)
 );
 
+CREATE TABLE tbl_fichier(
+    fichier_id int,
+    fichier_nom varchar(30),
+    contenu varchar(100),
+
+    PRIMARY KEY (fichier_id)
+);
+
+CREATE TABLE tbl_fichierPost(
+    fichier_id int,
+    post_id int,
+    
+    CONSTRAINT PK_tbl_fichierPost PRIMARY KEY
+    (
+        fichier_id,
+        post_id 
+    ),
+    FOREIGN KEY (post_id) REFERENCES tbl_post(post_id)
+    FOREIGN KEY (fichier_id) REFERENCES tbl_fichier(fichier_id)
+);
+
 INSERT INTO tbl_accountType VALUES (
     0,
-    'Administrator'
+    'administrator'
 );
 INSERT INTO tbl_accountType VALUES (
     1,
@@ -55,11 +115,15 @@ INSERT INTO tbl_accountType VALUES (
 );
 INSERT INTO tbl_accountType VALUES (
     2,
-    'compteNormal'
+    'etudiant'
 );
 INSERT INTO tbl_accountType VALUES (
     3,
     'professeur'
+);
+INSERT INTO tbl_accountType VALUES (
+    3,
+    'ancienEtudiant'
 );
 
 INSERT INTO tbl_user VALUES (
@@ -68,6 +132,7 @@ INSERT INTO tbl_user VALUES (
     "Admin",
     0,
     "root",
+    FALSE
 );
 INSERT INTO tbl_user VALUES (
     2,
@@ -75,6 +140,7 @@ INSERT INTO tbl_user VALUES (
     "Invité",
     1,
     "",
+    FALSE
 );
 INSERT INTO tbl_user VALUES (
     3,
@@ -82,6 +148,7 @@ INSERT INTO tbl_user VALUES (
     "Jérémy",
     2,
     "RalphBisou",
+    FALSE
 );
 INSERT INTO tbl_user VALUES (
     4,
@@ -89,6 +156,7 @@ INSERT INTO tbl_user VALUES (
     "Ralph Christian",
     2,
     "Qwerty123$",
+    TRUE
 );
 INSERT INTO tbl_user VALUES (
     5,
@@ -96,26 +164,78 @@ INSERT INTO tbl_user VALUES (
     "Daddy",
     3,
     "Qwerty123$",
+    FALSE
 );
 
 INSERT INTO tbl_projet VALUES (
     1,
     "test",
     3,
+    FALSE
+);
+
+INSERT INTO tbl_acces VALUES (
     1,
+    "Guest"
+);
+INSERT INTO tbl_acces VALUES (
+    2,
+    "Reporter"
+);
+INSERT INTO tbl_acces VALUES (
+    3,
+    "Developer"
+);
+INSERT INTO tbl_acces VALUES (
+    4,
+    "Maintainer"
+);
+INSERT INTO tbl_acces VALUES (
+    5,
+    "Owner"
 );
 
 INSERT INTO tbl_post VALUES (
     1,
     1,
     "Premier post du projet pour daddy dini",
-    "mettre le fichier ici",
     3,
+    '20120618 10:34:09 AM'
 );
 INSERT INTO tbl_post VALUES (
     2,
     1,
     "Ajout dans le projet",
-    "mettre le fichier ici",
     4,
+    '20120612 03:34:09 PM'
 );
+
+INSERT INTO tbl_fichier(
+    1,
+    "chat.png",
+    "TestTestTestTest"
+);
+INSERT INTO tbl_fichier(
+    2,
+    "rigolo.docx",
+    "hahahahahah"
+);
+INSERT INTO tbl_fichier(
+    3,
+    "betaDasa.sql",
+    "Jesaispasquoimettre"
+);
+
+INSERT INTO tbl_fichierPost(
+    1,
+    1
+);
+INSERT INTO tbl_fichierPost(
+    2,
+    1
+);
+INSERT INTO tbl_fichierPost(
+    3,
+    2
+);
+
