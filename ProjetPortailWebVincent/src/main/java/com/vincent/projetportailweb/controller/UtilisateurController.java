c23ff897061d5a4eb9c8c5a30ee236e99c3a6ca5
Vincent Huynh <2089020@crosemont.qc.ca>
2023-05-23 16:49:06 -0400

    Liste des etudiants à liste des utilisateurs

--- ProjetPortailWebVincent/src/main/java/com/vincent/projetportailweb/controller/UtilisateurController.java
+++ ProjetPortailWebVincent/src/main/java/com/vincent/projetportailweb/controller/UtilisateurController.java
@@ -1,7 +1,6 @@
 package com.vincent.projetportailweb.controller;
 
 import com.vincent.projetportailweb.entities.AccountType;
-import com.vincent.projetportailweb.entities.Fichier;
 
 import com.vincent.projetportailweb.entities.Utilisateur;
 import com.vincent.projetportailweb.service.UtilisateurNotFoundException;
@@ -9,12 +8,8 @@ import com.vincent.projetportailweb.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -26,11 +21,11 @@ public class UtilisateurController {
  @Autowired
  UtilisateurService service;
 
-    @GetMapping("/listeEtudiant")
+    @GetMapping("/listeUtilisateurs")
     public String afficherUtilisateur(Model model){
         Iterable<Utilisateur> listUtilisateurs = service.afficherUtilisateurs();
         model.addAttribute("listUtilisateurs",listUtilisateurs);
-        return "listeEtudiant";
+        return "listeUtilisateurs";
     }
 
     @GetMapping("/utilisateurs/new")
