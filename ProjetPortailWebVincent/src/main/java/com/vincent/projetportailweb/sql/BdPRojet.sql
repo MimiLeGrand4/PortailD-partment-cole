699052cd9bcc1008cc5da983824b00df7a8e49c8
Jérémy Cormier1 <jcormier1@dept-info.crosemont.quebec>
2023-05-23 14:53:47 -0400

    bd comme avant pcq bug

--- ProjetPortailWebVincent/src/main/java/com/vincent/projetportailweb/sql/BdPRojet.sql
+++ ProjetPortailWebVincent/src/main/java/com/vincent/projetportailweb/sql/BdPRojet.sql
@@ -74,57 +74,227 @@ CREATE TABLE tbl_fichier(
                             FOREIGN KEY (user_id) REFERENCES tbl_user(id)
 );
 
---tbl_accountType
-INSERT INTO tbl_accountType VALUES (0, 'administrator');
-INSERT INTO tbl_accountType VALUES (1, 'invite');
-INSERT INTO tbl_accountType VALUES (2, 'etudiant');
-INSERT INTO tbl_accountType VALUES (3, 'professeur');
-INSERT INTO tbl_accountType VALUES (4, 'ancienEtudiant');
+INSERT INTO tbl_accountType VALUES (
+                                       0,
+                                       'administrator'
+                                   );
+INSERT INTO tbl_accountType VALUES (
+                                       1,
+                                       'invite'
+                                   );
+INSERT INTO tbl_accountType VALUES (
+                                       2,
+                                       'etudiant'
+                                   );
+INSERT INTO tbl_accountType VALUES (
+                                       3,
+                                       'professeur'
+                                   );
+INSERT INTO tbl_accountType VALUES (
+                                       4,
+                                       'ancienEtudiant'
+                                   );
 
--- for tbl_session
-INSERT INTO tbl_session VALUES (0, 'Automne2010');
-INSERT INTO tbl_session VALUES (1, 'Hiver2011');
-INSERT INTO tbl_session VALUES (2, 'Automne2011');
-INSERT INTO tbl_session VALUES (3, 'Hiver2012');
-INSERT INTO tbl_session VALUES (4, 'Automne2012');
-INSERT INTO tbl_session VALUES (5, 'Hiver2013');
-INSERT INTO tbl_session VALUES (6, 'Automne2013');
-INSERT INTO tbl_session VALUES (7, 'Hiver2014');
-INSERT INTO tbl_session VALUES (8, 'Automne2014');
-INSERT INTO tbl_session VALUES (9, 'Hiver2015');
-INSERT INTO tbl_session VALUES (10, 'Automne2015');
-INSERT INTO tbl_session VALUES (11, 'Hiver2016');
-INSERT INTO tbl_session VALUES (12, 'Automne2016');
-INSERT INTO tbl_session VALUES (13, 'Hiver2017');
-INSERT INTO tbl_session VALUES (14, 'Automne2017');
-INSERT INTO tbl_session VALUES (15, 'Hiver2018');
-INSERT INTO tbl_session VALUES (16, 'Automne2018');
-INSERT INTO tbl_session VALUES (17, 'Hiver2019');
-INSERT INTO tbl_session VALUES (18, 'Automne2019');
-INSERT INTO tbl_session VALUES (19, 'Hiver2020');
-INSERT INTO tbl_session VALUES (20, 'Automne2020');
-INSERT INTO tbl_session VALUES (21, 'Hiver2021');
-INSERT INTO tbl_session VALUES (22, 'Automne2021');
-INSERT INTO tbl_session VALUES (23, 'Hiver2022');
-INSERT INTO tbl_session VALUES (24, 'Automne2022');
-INSERT INTO tbl_session VALUES (25, 'Hiver2023');
 
---  tbl_profil
-INSERT INTO tbl_profil VALUES (0, 'Réseautique');
-INSERT INTO tbl_profil VALUES (1, 'Programmation');
+INSERT INTO tbl_session VALUES (
+                                   1,
+                                   'Automne2010'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   2,
+                                   'Hiver2011'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   3,
+                                   'Automne2011'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   4,
+                                   'Hiver2012'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   5,
+                                   'Automne2012'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   6,
+                                   'Hiver2013'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   7,
+                                   'Automne2013'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   8,
+                                   'Hiver2014'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   9,
+                                   'Automne2014'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   10,
+                                   'Hiver2015'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   11,
+                                   'Automne2015'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   12,
+                                   'Hiver2016'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   13,
+                                   'Automne2016'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   14,
+                                   'Hiver2017'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   15,
+                                   'Automne2017'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   16,
+                                   'Hiver2018'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   17,
+                                   'Automne2018'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   18,
+                                   'Hiver2019'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   19,
+                                   'Automne2019'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   20,
+                                   'Hiver2020'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   21,
+                                   'Automne2020'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   22,
+                                   'Hiver2021'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   23,
+                                   'Automne2021'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   24,
+                                   'Hiver2022'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   25,
+                                   'Automne2022'
+                               );
+INSERT INTO tbl_session VALUES (
+                                   26,
+                                   'Hiver2023'
+                               );
 
--- tbl_user
-INSERT INTO tbl_user VALUES (1, 'Admin', 'Admin', 0, 'root', FALSE, 'root');
-INSERT INTO tbl_user VALUES (2, 'Invité', 'Invité', 1, '', FALSE, '');
-INSERT INTO tbl_user VALUES (3, 'Cormier', 'Jérémy', 2, 'RalphBisou', FALSE, 'jcormier@gmail.com');
-INSERT INTO tbl_user VALUES (4, 'Gabriel', 'Ralph Christian', 2, 'Qwerty123$', TRUE, 'rgabriel@gmail.com');
-INSERT INTO tbl_user VALUES (5, 'Dini', 'Daddy', 3, 'Qwerty123$', FALSE, 'ddini@gmail.com');
 
--- tbl_projet
-INSERT INTO tbl_projet VALUES (1, 'test', 'Lalalalalalalalalalalala description', 25, 1, 3, FALSE, 'Video1');
-INSERT INTO tbl_projet VALUES (2, 'testdutest', 'Lalalalalalalalalalalala description', 18, 0, 4, FALSE, 'Video2');
+INSERT INTO tbl_profil VALUES (
+                                  1,
+                                  'Réseautique'
+                              );
+INSERT INTO tbl_profil VALUES (
+                                  2,
+                                  'Programmation'
+                              );
 
---  tbl_fichier
-INSERT INTO tbl_fichier VALUES (1, 'chat.png', 'TestTestTestTest', 3);
-INSERT INTO tbl_fichier VALUES (2, 'rigolo.docx', 'hahahahahah', 5);
-INSERT INTO tbl_fichier VALUES (3, 'betaDasa.sql', 'Jesaispasquoimettre', 5);
+INSERT INTO tbl_user VALUES (
+                                1,
+                                "Admin",
+                                "Admin",
+                                0,
+                                "root",
+                                FALSE,
+                                "root"
+                            );
+INSERT INTO tbl_user VALUES (
+                                2,
+                                "Invité",
+                                "Invité",
+                                1,
+                                "",
+                                FALSE,
+                                ""
+                            );
+INSERT INTO tbl_user VALUES (
+                                3,
+                                "Cormier",
+                                "Jérémy",
+                                2,
+                                "RalphBisou",
+                                FALSE,
+                                "jcormier@gmail.com"
+                            );
+INSERT INTO tbl_user VALUES (
+                                4,
+                                "Gabriel",
+                                "Ralph Christian",
+                                2,
+                                "Qwerty123$",
+                                TRUE,
+                                "rgabriel@gmail.com"
+                            );
+INSERT INTO tbl_user VALUES (
+                                5,
+                                "Dini",
+                                "Daddy",
+                                3,
+                                "Qwerty123$",
+                                FALSE,
+                                "ddini@gmail.com"
+                            );
+
+INSERT INTO tbl_projet VALUES (
+                                  1,
+                                  "test",
+                                  "Lalalalalalalalalalalala description",
+                                  25,
+                                  1,
+                                  3,
+                                  FALSE,
+                                  "Video1"
+                              );
+INSERT INTO tbl_projet VALUES (
+                                  2,
+                                  "testdutest",
+                                  "Lalalalalalalalalalalala description",
+                                  18,
+                                  2,
+                                  4,
+                                  FALSE,
+                                  "Video2"
+                              );
+
+INSERT INTO tbl_fichier VALUES(
+                                  1,
+                                  "chat.png",
+                                  "TestTestTestTest",
+                                  3
+                              );
+INSERT INTO tbl_fichier VALUES(
+                                  2,
+                                  "rigolo.docx",
+                                  "hahahahahah",
+                                  5
+                              );
+INSERT INTO tbl_fichier VALUES(
+                                  3,
+                                  "betaDasa.sql",
+                                  "Jesaispasquoimettre",
+                                  5
+                              );
+
+
